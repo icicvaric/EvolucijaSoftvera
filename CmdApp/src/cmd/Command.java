ffc61297eb1fbbe2060645a638f6ce780081e8c4
Ivana <icacicvaric@gmail.com>
2020-09-20 13:25:32 +0200

    Changing methods to be static

--- CmdApp/src/cmd/Command.java
+++ CmdApp/src/cmd/Command.java
@@ -1,5 +1,15 @@
 package cmd;
 
-public class Command {
+public abstract class Command {
+
+	public static Object ParseCommand(Object object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Object GetAbsoluthePath(Object object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- CmdApp/src/test/CommandTest.java
+++ CmdApp/src/test/CommandTest.java
@@ -7,23 +7,17 @@ import org.junit.jupiter.api.Test;
 import cmd.Command;
 
 class CommandTest {
-
-	@Test
-	void CanInstantiateCommand() {
-		Command command = new Command();
-		assertNotNull(command);
-	}
 	
 	@Test
 	void ParseCommandTest() {
-		Command command = new Command();
-		assertNull(command.ParseCommand(null));
+		
+		assertNull(Command.ParseCommand(null));
 	}
 	
 	@Test
 	void GetAbsoluthePathTest() {
-		Command command = new Command();
-		assertNull(command.GetAbsoluthePath(null));
+		
+		assertNull(Command.GetAbsoluthePath(null));
 	}
 
 }
