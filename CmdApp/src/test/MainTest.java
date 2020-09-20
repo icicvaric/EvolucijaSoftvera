ff0d605c2f8b2c63a08752046cb8cca64c847bc1
Ivana <icacicvaric@gmail.com>
2020-09-20 21:29:44 +0200

    Adding test for initCommandMapFunction

--- CmdApp/src/test/MainTest.java
+++ CmdApp/src/test/MainTest.java
@@ -2,13 +2,33 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 
 class MainTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void InitCommandMapTest() {
+		
+		assertNotNull(Main.commandMap);
+		
+		Main.initCommandMap();
+		
+		assertNotNull(Main.commandMap.keySet());
+		assertTrue(Main.commandMap.size() > 0);
+		
+		Set<String> keySet = Main.commandMap.keySet();
+		
+		assertTrue(keySet.contains("cd"));
+		assertTrue(keySet.contains("compare"));
+		assertTrue(keySet.contains("copy"));
+		assertTrue(keySet.contains("create"));
+		assertTrue(keySet.contains("delete"));
+		assertTrue(keySet.contains("help"));
+		assertTrue(keySet.contains("dir"));
+		assertTrue(keySet.contains("move"));
+		assertTrue(keySet.contains("rename"));
 	}
 
 }
