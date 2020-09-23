0fda0cec2e2b99daa420362026884a187a9ba2f5
Ivana <icacicvaric@gmail.com>
2020-09-23 18:47:07 +0200

    Adding test for delete dir - test fails

--- CmdApp/src/test/DeleteCommandTest.java
+++ CmdApp/src/test/DeleteCommandTest.java
@@ -1,8 +1,12 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import org.junit.jupiter.api.Test;
-import cmd.DeleteCommand;
+import cmd.*;
 
 class DeleteCommandTest {
 
@@ -13,7 +17,27 @@ class DeleteCommandTest {
 	}
 	
 	@Test
-	void PerformCommandTest() {
+	void PerformCommandTest() throws IOException {
+		DeleteCommand deleteCommand = new DeleteCommand();
 		
+		assertThrows(IOException.class, () -> deleteCommand.PerformCommand("  "));
+		assertThrows(IOException.class, () -> deleteCommand.PerformCommand("delete"));
+		assertThrows(IOException.class, () -> deleteCommand.PerformCommand("delete folder1 folder2"));
+
+		HelperMethods.CreateDirForTest();		
+		assertTrue(Files.exists( HelperMethods.absPath));
+		deleteCommand.PerformCommand("delete " + HelperMethods.absPath);
+		assertFalse(Files.exists( HelperMethods.absPath));
+		
+		HelperMethods.CreateFileForTest();		
+		assertTrue(Files.exists( HelperMethods.fileAbsPath));
+		deleteCommand.PerformCommand("delete " + HelperMethods.fileAbsPath);
+		assertFalse(Files.exists( HelperMethods.fileAbsPath));
+		
+		HelperMethods.CreateNonEmptyDirForTest();		
+		assertTrue(Files.exists( HelperMethods.fileAbsPath));
+		assertTrue(Files.exists( HelperMethods.fileAbsPath.resolve(HelperMethods.fileName) ) );
+		deleteCommand.PerformCommand("delete " + HelperMethods.fileAbsPath);
+		assertFalse(Files.exists( HelperMethods.fileAbsPath));
 	}
 }
--- CmdApp/src/test/HelperMethods.java
+++ CmdApp/src/test/HelperMethods.java
@@ -1,12 +1,15 @@
 package test;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 
 public class HelperMethods {
 	
+	public static String fileName = "file.txt";
 	public static Path absPath = Paths.get(System.getProperty("user.dir").toString()).toAbsolutePath().resolve("temp");
-	
+	public static Path fileAbsPath = Paths.get(System.getProperty("user.dir").toString()).toAbsolutePath().resolve(fileName);
+
 	public static void CreateDirForTest() throws IOException
 	{
 		Files.createDirectory(absPath);
@@ -15,6 +18,22 @@ public class HelperMethods {
 	public static void DeleteDirForTest() throws IOException
 	{
 		Files.delete(absPath);
+	}
+	
+	public static void CreateFileForTest() 
+	{
+		new File(fileAbsPath.toString());
+	}
+	
+	public static void DeleteFileForTest()
+	{
+		new File(fileAbsPath.toString()).delete();
+	}
+	
+	public static void CreateNonEmptyDirForTest() throws IOException
+	{
+		Files.createDirectory(absPath);
+		new File( absPath.toString() + File.pathSeparator + fileName);
 	}
 
 }
