9cb1896ea4d347ed482f1d1bc9c2039e8299ee82
Ivana <icacicvaric@gmail.com>
2020-09-23 22:59:52 +0200

    Adding implementation for rename dir command - test passes

--- CmdApp/src/cmd/RenameCommand.java
+++ CmdApp/src/cmd/RenameCommand.java
@@ -1,10 +1,26 @@
 package cmd;
 
+import java.io.File;
+import java.io.IOException;
+
 public class RenameCommand extends Command {
 
 	@Override
-	public void PerformCommand(String command) {
-		// TODO Auto-generated method stub
+	public void PerformCommand(String command) throws IOException {
+		String[] parsedCommand = ParseCommand(command);
+		
+		if( parsedCommand == null || parsedCommand.length != 3)
+			throw new IOException("Invalid number of arguments!");
+		
+		String absPath = GetAbsoluthePath(parsedCommand[1]);
+		File dir = new File(absPath);
+		
+		if (!dir.isDirectory()) {
+			  System.err.println("There is no directory @ given path");
+		} else {
+			File newDir = new File(dir.getParent() + File.separator + parsedCommand[2]);
+		    dir.renameTo(newDir);
+		}
 		
 	}
 
