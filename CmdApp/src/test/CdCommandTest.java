d7d7dab398c35c4f31367928ea69d9e858d306c3
Ivana <icacicvaric@gmail.com>
2020-09-24 21:05:11 +0200

    Adding test for cd command

--- CmdApp/src/test/CdCommandTest.java
+++ CmdApp/src/test/CdCommandTest.java
@@ -1,6 +1,10 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.Test;
 import cmd.CdCommand;
 
@@ -13,8 +17,26 @@ class CdCommandTest {
 	}
 	
 	@Test
-	void PerformCommandTest() {
+	void PerformCommandTest() throws IOException {
+		CdCommand cdCommand = new CdCommand();
 		
+		assertThrows(IOException.class, () -> cdCommand.PerformCommand("  "));
+		assertThrows(IOException.class, () -> cdCommand.PerformCommand("cd"));
+		assertThrows(IOException.class, () -> cdCommand.PerformCommand("cd folder1 folder2"));
+
+		String userDir = System.getProperty("user.dir");
+		cdCommand.PerformCommand("cd ..");
+		assertEquals(Paths.get(userDir).getParent().toString(), System.getProperty("user.dir"));		
+		System.setProperty("user.dir", userDir);
+		
+		HelperMethods.CreateDirForTest();
+		cdCommand.PerformCommand("cd temp");
+		assertEquals(Paths.get(userDir).resolve("temp").toString(), System.getProperty("user.dir"));
+		System.setProperty("user.dir", userDir);
+		HelperMethods.DeleteDirForTest();
+		
+		cdCommand.PerformCommand("cd unkonwn");
+		assertEquals(userDir, System.getProperty("user.dir"));
 	}
 
 }
