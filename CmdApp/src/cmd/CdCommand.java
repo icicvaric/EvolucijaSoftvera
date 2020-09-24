cc41a965816a82ad9ad4183f620d0493f695d226
Ivana <icacicvaric@gmail.com>
2020-09-24 21:10:03 +0200

    Adding implementation for cd command - test passes

--- CmdApp/src/cmd/CdCommand.java
+++ CmdApp/src/cmd/CdCommand.java
@@ -1,11 +1,24 @@
 package cmd;
 
+import java.io.File;
+import java.io.IOException;
+
 public class CdCommand extends Command {
 
 	@Override
-	public void PerformCommand(String command) {
-		// TODO Auto-generated method stub
+	public void PerformCommand(String command) throws IOException {
+		String[] parsedCommand = ParseCommand(command);
 		
+		if(parsedCommand == null || parsedCommand.length != 2)
+			throw new IOException("Invalid number of arguments!");
+		
+		String absPath = GetAbsoluthePath(parsedCommand[1]);
+		File fileToChange = new File(absPath);
+		
+		if(!fileToChange.exists())
+			return;
+		
+		System.setProperty("user.dir", absPath);
 	}
 
 }
