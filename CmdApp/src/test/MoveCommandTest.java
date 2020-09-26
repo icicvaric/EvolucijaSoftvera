affd66f4e76d57deafca5ae952305423805006cf
Ivana <icacicvaric@gmail.com>
2020-09-26 19:59:30 +0200

    Adding test for move command - test fails

--- CmdApp/src/test/MoveCommandTest.java
+++ CmdApp/src/test/MoveCommandTest.java
@@ -1,6 +1,8 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+import java.nio.file.Files;
 import org.junit.jupiter.api.Test;
 import cmd.MoveCommand;
 
@@ -13,7 +15,34 @@ class MoveCommandTest {
 	}
 
 	@Test
-	void PerformCommandTest() {
+	void PerformCommandTest() throws IOException {
+		MoveCommand moveCommand = new MoveCommand();
 		
+		assertThrows(IOException.class, () -> moveCommand.PerformCommand("  "));
+		assertThrows(IOException.class, () -> moveCommand.PerformCommand("move "));
+		assertThrows(IOException.class, () -> moveCommand.PerformCommand("move folder1"));
+		assertThrows(IOException.class, () -> moveCommand.PerformCommand("move folder1 folder2 folder3"));
+
+		HelperMethods.CreateDirForTest();
+		HelperMethods.CreateFileForTest();		
+		moveCommand.PerformCommand("move " + HelperMethods.fileName + " " + HelperMethods.folderName);				
+		// check if file is deleted from old location
+		assertFalse( Files.isReadable(HelperMethods.fileAbsPath));
+		// check if file is on new location
+		assertTrue( Files.isReadable(HelperMethods.absPath.resolve(HelperMethods.fileName)));
+		
+		// we cannot move something to file
+		assertThrows(IOException.class, () -> moveCommand.PerformCommand("move " + HelperMethods.folderName + " " + HelperMethods.fileName));	
+		// we cannot move folder that doesn't exist
+		assertThrows(IOException.class, () -> moveCommand.PerformCommand("move " + "FolderThatDoesntExist" + " " + HelperMethods.folderName));
+		
+		HelperMethods.CreateDirForTest(HelperMethods.absPath1);
+		moveCommand.PerformCommand("move " + HelperMethods.folderName1 + " " + HelperMethods.folderName);
+		// check if folder is deleted from old location
+		assertFalse( Files.isReadable(HelperMethods.absPath1));
+		// check if folder is on new location
+		assertTrue( Files.isReadable(HelperMethods.absPath.resolve(HelperMethods.folderName1)));			
+		
+		HelperMethods.DeleteNonEmptyDirForTest();
 	}
 }
