ad84509e1cf5205b3bf8d84db456b98eae01df38
Ivana <icacicvaric@gmail.com>
2020-09-26 16:57:30 +0200

    Adding test for help command - test fails

--- CmdApp/src/test/HelpCommandTest.java
+++ CmdApp/src/test/HelpCommandTest.java
@@ -1,7 +1,12 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
+
+import cmd.CreateCommand;
 import cmd.HelpCommand;
 
 class HelpCommandTest {
@@ -14,6 +19,27 @@ class HelpCommandTest {
 
 	@Test
 	void PerformCommandTest() {
+		HelpCommand helpCommand = new HelpCommand();
 		
+		assertThrows(IOException.class, () -> helpCommand.PerformCommand("  "));
+		assertThrows(IOException.class, () -> helpCommand.PerformCommand("help file1"));
+		assertDoesNotThrow(() -> helpCommand.PerformCommand("help"));
+	}
+	
+	@Test
+	void GetDetailsTest() {
+		HelpCommand helpCommand = new HelpCommand();
+		String commandDetails = helpCommand.GetCommandDetails().toLowerCase();
+		assertTrue( commandDetails != null);
+		assertFalse( commandDetails.isEmpty() );
+		assertFalse( commandDetails.isBlank() );
+		assertTrue( commandDetails.contains("cd") );
+		assertTrue( commandDetails.contains("compare") );
+		assertTrue( commandDetails.contains("copy") );
+		assertTrue( commandDetails.contains("create") );
+		assertTrue( commandDetails.contains("delete") );
+		assertTrue( commandDetails.contains("dir") );
+		assertTrue( commandDetails.contains("move") );
+		assertTrue( commandDetails.contains("rename") );
 	}
 }
