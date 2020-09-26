c73a642b746e5664d32b96dbd03f9475e85d4f83
Ivana <icacicvaric@gmail.com>
2020-09-26 22:25:48 +0200

    Adding test for compare command - test fails

--- CmdApp/src/test/CompareCommandTest.java
+++ CmdApp/src/test/CompareCommandTest.java
@@ -1,8 +1,15 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.graalvm.compiler.nodes.spi.SwitchFoldable.Helper;
 import org.junit.jupiter.api.Test;
 import cmd.CompareCommand;
+import cmd.MoveCommand;
 
 class CompareCommandTest {
 
@@ -14,7 +21,31 @@ class CompareCommandTest {
 	
 	@Test
 	void PerformCommandTest() {
+		CompareCommand compareCommand = new CompareCommand();
 		
+		assertThrows(IOException.class, () -> compareCommand.PerformCommand("  "));
+		assertThrows(IOException.class, () -> compareCommand.PerformCommand("compare "));
+		assertThrows(IOException.class, () -> compareCommand.PerformCommand("compare folder1"));
+		assertThrows(IOException.class, () -> compareCommand.PerformCommand("compare folder1 folder2 folder3"));
+
+		HelperMethods.CreateNonEmptyDirForTest();
+		
+		HelperMethods.CreateFileForTest();	
+		// we cannot compare folder and file, only 2 folders can  be compared
+		assertThrows(IOException.class, () ->compareCommand.PerformCommand("compare " + HelperMethods.fileName + " " + HelperMethods.folderName));	
+		assertThrows(IOException.class, () ->compareCommand.PerformCommand("compare " + HelperMethods.folderName + " " + HelperMethods.fileName));			
+		HelperMethods.DeleteFileForTest();
+		
+		HelperMethods.CreateDirForTest(HelperMethods.absPath1);
+		
+		String difference = compareCommand.GetDifference(new File(HelperMethods.absPath.toString()), new File(HelperMethods.absPath1.toString()));
+		assertTrue(difference != null);
+		assertTrue(!difference.isEmpty());
+		assertTrue(!difference.isBlank());
+		assertEquals(HelperMethods.fileName, difference);
+	
+		HelperMethods.DeleteNonEmptyDirForTest();
+		HelperMethods.DeleteDirForTest(HelperMethods.absPath1);
 	}
 
 }
