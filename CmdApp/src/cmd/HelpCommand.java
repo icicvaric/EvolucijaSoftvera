0911ce74350b553e2af73dfd81ad7c7156af719c
Ivana <icacicvaric@gmail.com>
2020-09-26 22:49:05 +0200

    Modifying help command output so it looks better

--- CmdApp/src/cmd/HelpCommand.java
+++ CmdApp/src/cmd/HelpCommand.java
@@ -17,14 +17,14 @@ public class HelpCommand extends Command {
 	public String GetCommandDetails()
 	{
 		StringBuilder manual = new StringBuilder();
-		manual.append("CD command help => cd .. (change to parent) ; cd folderPath"); manual.append(System.lineSeparator());		
+		manual.append("CD      command help => cd .. (change to parent) ; cd folderPath"); manual.append(System.lineSeparator());		
 		manual.append("COMPARE command help => compare folderPath folderPath"); manual.append(System.lineSeparator());
-		manual.append("COPY command help => copy source(filePath or folderPath) destination(folderPath)"); manual.append(System.lineSeparator());
-		manual.append("CREATE command help => create folderPath"); manual.append(System.lineSeparator());
-		manual.append("DELETE command help => delete folderPath ; delete filePath"); manual.append(System.lineSeparator());
-		manual.append("DIR command help => dir . (for current folder) ; dir folderPath"); manual.append(System.lineSeparator());
-		manual.append("MOVE command help => move source(filePath or folderPath) destination(folerPath)"); manual.append(System.lineSeparator());
-		manual.append("RENAME command help => rename folderPath newName"); manual.append(System.lineSeparator());
+		manual.append("COPY    command help => copy source(filePath or folderPath) destination(folderPath)"); manual.append(System.lineSeparator());
+		manual.append("CREATE  command help => create folderPath"); manual.append(System.lineSeparator());
+		manual.append("DELETE  command help => delete folderPath ; delete filePath"); manual.append(System.lineSeparator());
+		manual.append("DIR     command help => dir . (for current folder) ; dir folderPath"); manual.append(System.lineSeparator());
+		manual.append("MOVE    command help => move source(filePath or folderPath) destination(folerPath)"); manual.append(System.lineSeparator());
+		manual.append("RENAME  command help => rename folderPath newName"); manual.append(System.lineSeparator());
 		return manual.toString();
 	}
 }
